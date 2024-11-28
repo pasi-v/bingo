da7fa3371dbbba3b5c2b33e63ab3edc6aa3125ca
Pasi Välkkynen <pasi.valkkynen@iki.fi>
2024-11-28 15:30:56 +0200

    Lord of the Rings topic

---
+++ inst/topics/lotr.R
@@ -0,0 +1,28 @@
+# Lord of the Rings (movie) themed bingo
+
+c(
+  "Frodo almost loses the Ring",
+  "Sam saves Frodo's life",
+  "Gollum says 'Precious'",
+  "Aragorn broods dramatically",
+  "Legolas shoots something mid-air",
+  "Gimli brags about his axe",
+  "Boromir gets tempted by the Ring",
+  "A Hobbit mentions food",
+  "Gandalf gives cryptic advice",
+  "Elrond looks disappointed",
+  "Someone dramatically stares into the distance",
+  "The Fellowship walks over a mountain",
+  "Saruman shouting from Orthanc",
+  "Pippin causes trouble",
+  "Gandalf gets frustrated because of Pippin",
+  "Legolas says something profound",
+  "Gimli makes a short joke",
+  "Arwen glows ethereally",
+  "The Nazgûl scream",
+  "A dramatic speech before battle",
+  "Faramir doesn’t get enough credit",
+  "A Gondorian soldier looks scared",
+  "Someone says 'It is hopeless!'",
+  "The Ring whispers creepily"
+)
